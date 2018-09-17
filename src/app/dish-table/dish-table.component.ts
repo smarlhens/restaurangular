f1f911272b8118db0530055a062ccfd69e57d5d0
Samuel Marlhens <samuel.marlhens@modis.com>
2018-09-17 11:56:11 +0200

    Added price in dish table

--- src/app/dish-table/dish-table.component.ts
+++ src/app/dish-table/dish-table.component.ts
@@ -13,7 +13,7 @@ export class DishTableComponent implements OnInit {
   dataSource: DishTableDataSource;
 
   /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
-  displayedColumns = ['id', 'name'];
+  displayedColumns = ['name', 'price'];
 
   ngOnInit() {
     this.dataSource = new DishTableDataSource(this.paginator, this.sort);
